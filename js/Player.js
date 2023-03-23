9a2cb8b676a574582d777fd69f9eafa8ecc100dc
JORGE-MEGEREDO <114191444+JORGE-MEGEREDO@users.noreply.github.com>
2023-03-23 18:27:59 -0300

    Update Player.js

--- js/Player.js
+++ js/Player.js
@@ -1,21 +1,20 @@
 class Player {
   constructor() {
-    this.name = null;
-    this.index = null;
-    this.positionX = 0;
-    this.positionY = 0;
+    this.name = null; //esse nome é vazio (null) inicialmente
+    this.index = null;5 //esse indice (a quantia de jogadores registrada) é vazio inicialmente
+    this.positionX = 0; //a posição x do jogador é zerada inicialmente
+    this.positionY = 0;  //a posição y do jogador é zerada inicialmente
   }
 
-   addPlayer() {
-    var playerIndex = "players/player" + this.index;
-
-    if (this.index === 1) {
+   addPlayer() {   { //adiciona o jogador  
+    var playerIndex = "players/player" + this.index; //cria o indice/posição a partir das informações que temos 
+    if (this.index === 1) { //se for o primeiro jogador a posição x será metade da largura subtraindo 100 
       this.positionX = width / 2 - 100;
-    } else {
+    } else {    //se for o segundo jogador a posição x será metade da largura adicionando 100
       this.positionX = width / 2 + 100;
     }
 
-    database.ref(playerIndex).set({
+    database.ref(playerIndex).set({  //se for o segundo jogador a posição x será metade da largura adicionando 100
       name: this.name,
       positionX: this.positionX,
       positionY: this.positionY,
